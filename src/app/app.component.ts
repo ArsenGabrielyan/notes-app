21a4efc58b63c39f1c7a137a265ea49997f0f28f
ArsenGabrielyan <arsentech5002@gmail.com>
2023-01-16 15:47:41 +0400

    Initialized, But Not Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Note } from './note';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'notes-app';
+export class AppComponent implements OnInit{
+  notesForm!: FormGroup;
+  notes: Note[] = JSON.parse(localStorage.getItem('notes')!) || [];
+
+  constructor(private fbuild: FormBuilder){}
+  ngOnInit():void{this.notesForm = this.fbuild.group({title: [''],note: ['']})}
+  addNote(){
+    const {title,note} = this.notesForm.value;
+    let data: Note = {title: title,note:note}
+    this.notes.push(data);
+    localStorage.setItem('notes',JSON.stringify(this.notes));
+    this.notesForm.reset({title: "",note:""})
+  }
 }
