a78834f9be68cb11d31a8ca197139ae214027040
ArsenGabrielyan <arsentech5002@gmail.com>
2023-01-19 15:07:15 +0400

    Some Codes Are Cleaned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,32 +9,32 @@ import { Note } from './interfaces/note';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  notesForm!: FormGroup; editForm!: FormGroup;
-  editModal = false; viewModal = false;oldTitle!: string; oldNote!: string; 
+  frmNotes!: FormGroup; frmEdit!: FormGroup;
+  editMdl = false; viewMdl = false;oldTitle!: string; oldNote!: string; 
   viewTitle!: string; viewNote!: string; chosen!: number;
   notes: Note[] = JSON.parse(localStorage.getItem('notes')!) || [];
   deleted: Note[] = JSON.parse(localStorage.getItem('deleted')!) || [];
-  tabs = localStorage.getItem("tab") || "notes";
+  tab = localStorage.getItem("tab") || "notes";
   constructor(private fbuild: FormBuilder){}
   ngOnInit():void{
-    this.notesForm = this.fbuild.group({
+    this.frmNotes = this.fbuild.group({
       title: ['', [Validators.required, CustomValidation.spaceValidation]],
       note: ['',[Validators.required, CustomValidation.spaceValidation]]
     })
   }
-  switchTab(tab:string){this.tabs = tab;localStorage.setItem('tab',this.tabs);}
+  switchTab(t:string){this.tab = t;localStorage.setItem('tab',this.tab);}
   addNote(){
-    const {title,note} = this.notesForm.value;
+    const {title,note} = this.frmNotes.value;
     let data: Note = {title:title,note:note};this.notes.push(data);
     localStorage.setItem('notes',JSON.stringify(this.notes));
-    this.notesForm.reset({title:"",note:""})
+    this.frmNotes.reset({title:"",note:""})
   }
   editNote(){
-    const {newTitle,newNote} = this.editForm.value;
+    const {newTitle,newNote} = this.frmEdit.value;
     this.notes[this.chosen].title = newTitle;
     this.notes[this.chosen].note = newNote;
     localStorage.setItem('notes',JSON.stringify(this.notes));
-    this.editForm.reset({newTitle: "",newNote: "",});this.closeEditModal();
+    this.frmEdit.reset({newTitle: "",newNote: "",});this.closeEditModal();
   }
   deleteNote(i:number){
     this.deleted.push(this.notes[i]);this.notes.splice(i,1);
@@ -42,17 +42,17 @@ export class AppComponent implements OnInit{
     localStorage.setItem('deleted',JSON.stringify(this.deleted));
   }
   openEditModal(i:number){
-    this.editModal = true;
-    this.editForm = this.fbuild.group({
+    this.editMdl = true;
+    this.frmEdit = this.fbuild.group({
       newTitle: ["",[Validators.required, CustomValidation.spaceValidation]],
       newNote: ["",[Validators.required, CustomValidation.spaceValidation]]
     })
     this.oldTitle = this.notes[i].title;
     this.oldNote = this.notes[i].note;this.chosen = i;
   }
-  openViewModal(i:number){this.viewModal= true;this.viewTitle = this.notes[i].title;this.viewNote = this.notes[i].note;}
-  closeEditModal = () => this.editModal = false;
-  closeViewModal = () => this.viewModal = false;
+  openViewModal(i:number){this.viewMdl= true;this.viewTitle = this.notes[i].title;this.viewNote = this.notes[i].note;}
+  closeEditModal = () => this.editMdl = false;
+  closeViewModal = () => this.viewMdl = false;
   deleteForever(i:number){
     const sure = confirm("Are you sure to delete this note forever?");
     if(sure){this.deleted.splice(i,1);localStorage.setItem('deleted',JSON.stringify(this.deleted));}
